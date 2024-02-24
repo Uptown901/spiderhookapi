61bf6356be2276e0e57069b4df47a1bf8b1134c2
Joe Lee <lee.joseph893@gmail.com>
2024-02-24 10:14:33 -0500

    -Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_file, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+ 
+@app.route('/video')
+def play_video():
+    video_path = '/mnt/c/Users/standard/Documents/Video_Project/video/Volume_1.mp4'
+    return send_file(video_path, mimetype='video/mp4') 
+
+if __name__ == '__main__':
+    app.run(debug=True)
